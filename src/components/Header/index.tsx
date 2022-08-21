1dba38595634b63da7eaa41df5fb6c8465afde64
Andre Lima <andresdslima@gmail.com>
2022-08-21 19:39:14 +0200

    feat: header component ready

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
 import * as S from './styled';
+import logo from '../../assets/images/logo.jpg';
 
 export default function Header() {
-	return <div>Header</div>;
+	return (
+		<S.SContainer>
+			<S.SLogo src={logo} alt="Logo" />
+			<nav>
+				<S.SLink active to="/">
+					Pedidos
+				</S.SLink>
+				<S.SLink to="/">Cozinha</S.SLink>
+				<S.SLink to="/">Retirada</S.SLink>
+			</nav>
+		</S.SContainer>
+	);
 }
