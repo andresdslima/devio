a8390c464b8a0f2eba7285ddb4d75b73da316201
Andre Lima <andresdslima@gmail.com>
2022-08-25 00:22:01 +0200

    refactor: dynamix orders page ready

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,52 +1,38 @@
 import React from 'react';
-import { OrderProps } from '../../@types';
+import { useSelector } from 'react-redux';
+import { PersistedReducerProps } from '../../@types';
 import * as S from './styled';
 
 export default function OrderList() {
-	const allOrders: OrderProps[] = [
-		{
-			id: 1,
-			client: 'Lara',
-			payment: 'credito',
-			total: 110,
-			status: 'pending',
-		},
-		{
-			id: 2,
-			client: 'Andre',
-			comment: 'sem cebola',
-			payment: 'dinheiro',
-			change: 20,
-			total: 130,
-			status: 'pending',
-		},
-		{
-			id: 3,
-			client: 'Lucas',
-			payment: 'dinheiro',
-			change: 10,
-			total: 190,
-			status: 'ready',
-		},
-	];
+	const allOrders = useSelector(
+		(state: PersistedReducerProps) => state.persistedReducer.allOrders,
+	);
 
 	return (
 		<S.SContainer>
 			<S.SDiv border="3px solid #000">
 				<h2>Preparando:</h2>
-				{allOrders.map(
-					order =>
-						order.status === 'pending' && (
-							<h1 key={order.id}>{order.client}</h1>
-						),
-				)}
+				{allOrders.map(orderArray => {
+					const order = orderArray[orderArray.length - 1];
+
+					if (order.status === 'pending') {
+						return <h1 key={order.id}>{order.client}</h1>;
+					}
+
+					return null;
+				})}
 			</S.SDiv>
 			<div className="order-ready">
 				<h2>Pronto:</h2>
-				{allOrders.map(
-					order =>
-						order.status === 'ready' && <h1 key={order.id}>{order.client}</h1>,
-				)}
+				{allOrders.map(orderArray => {
+					const order = orderArray[orderArray.length - 1];
+
+					if (order.status === 'ready') {
+						return <h1 key={order.id}>{order.client}</h1>;
+					}
+
+					return null;
+				})}
 			</div>
 		</S.SContainer>
 	);
